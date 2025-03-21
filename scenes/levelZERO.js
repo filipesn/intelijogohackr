e811b99800e2bf6ca6d5565517456b394d17ac83
filipesn <40408605+filipesn@users.noreply.github.com>
2025-03-21 10:59:31 -0300

    update para semana 7

--- scenes/levelZERO.js
+++ scenes/levelZERO.js
@@ -1,36 +1,53 @@
 class LEVEL0 extends Phaser.Scene {
-//setup
+    //setup
     constructor() {
         super({
             key: 'LEVEL0',
             backgroundColor: '#000',
         });
     }
-//carregar imagens
+
+    //carregar imagens
     preload() {
         this.load.image("lev1", "assets/bg.png");
         this.load.image("sky2", "assets/sky2.png");
     }
-//cria o fundo e o texto
+
+    //cria o fundo e o texto
     create() {
         this.add.image(400, 300, 'sky2');
 
-        var text = { height: 20, padding: 15, content: "HACKFIELD" }
-        this.message = this.add.text(
-            this.game.config.width / 2,
-            this.game.config.height - 500,
-            text.content, {
-                color: "#FFFFFF",
-                fontSize: 40,
-                fontStyle: "bold"
-            }
-        ).setOrigin(0.5);
-//botao, que te leva para a proxima cena.
-        this.playBt = this.add.image(this.game.config.width / 2, this.game.config.height / 2, 'lev1')
-            .setScale(5).setOrigin(0.5, 0.5).setInteractive();
+        if (this.sys.game.device.os.desktop) {
+            var text = { height: 20, padding: 15, content: "HACKFIELD" };
+            this.message = this.add.text(
+                this.game.config.width / 2,
+                this.game.config.height - 500,
+                text.content, {
+                    color: "#FFFFFF",
+                    fontSize: "40px",
+                    fontStyle: "bold"
+                }
+            ).setOrigin(0.5);
 
-        this.playBt.on('pointerdown', function () {
-            this.scene.start('Level1');
-        }, this);
+            // Botão que te leva para a próxima cena
+            this.playBt = this.add.image(this.game.config.width / 2, this.game.config.height / 2, 'lev1')
+                .setScale(5).setOrigin(0.5, 0.5).setInteractive();
+
+            this.playBt.on('pointerdown', function () {
+                this.scene.start('Level1');
+            }, this);
+
+        } else {
+            var text = { height: 20, padding: 15, content: "jogo nao funciona em mobile :(" };
+            this.message = this.add.text(
+                this.game.config.width / 2,
+                this.game.config.height - 500,
+                text.content, {
+                    color: "#FFFFFF",
+                    fontSize: "30px",
+                    fontStyle: "bold"
+                }
+            ).setOrigin(0.5);
+        }
     }
 }
